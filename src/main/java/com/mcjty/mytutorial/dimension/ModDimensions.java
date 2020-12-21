7dc1a80ef8f2d6fa9eb2d7c8df29e568eb1e3b58
Cacofiin <Onidessu@gmail.com>
2020-12-21 14:42:48 +0100

    Base modification of gradle following a recent tutorial.

---
+++ src/main/java/com/mcjty/mytutorial/dimension/ModDimensions.java
@@ -0,0 +1,12 @@
+package com.mcjty.mytutorial.dimension;
+
+import com.mcjty.mytutorial.MyTutorial;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.dimension.DimensionType;
+
+public class ModDimensions {
+
+    public static final ResourceLocation DIMENSION_ID = new ResourceLocation(MyTutorial.MODID, "dimension");
+
+    public static DimensionType DIMENSION_TYPE;
+}
